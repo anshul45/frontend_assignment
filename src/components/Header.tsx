55a761385ef4d1ff93eb6074e67330e8820a1df6
Anshul45 <anshulgarwal025@gmail.com>
2024-08-11 23:04:24 +0530

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import Search from "./Search"
 const Header = () => {
   const [isOpen,setIsOpen] = useState(false);
   return (
-    <div className="flex justify-between items-center border-b-[1px] border-gray-500 h-[89px] xl:pl-40 xl:pr-32 xl:bg-yellow-100  lg:bg-green-100 lg:px-10 md:bg-orange-100 md:px-8   sm:px-16" >
+    <div className="flex justify-between items-center border-b-[1px] border-gray-500 h-[89px] xl:pl-40 xl:pr-32 lg:px-10  md:px-8   sm:px-16" >
       <div className="flex items-center md:gap-3.5 lg:gap-10">
         <Logo/>
         <Search/>
